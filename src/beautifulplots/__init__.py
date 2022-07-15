e676559a9f5b5e09cd8c2a166481cfd6322fbcf2
Al Gutierrez <aljgutier@yahoo.com>
2022-07-14 19:06:51 -0700

    fix: add set_yaxis_params to __init

--- src/beautifulplots/__init__.py
+++ src/beautifulplots/__init__.py
@@ -3,8 +3,9 @@ from importlib.metadata import version
 __version__ = version("beautifulplots")
 
 
-from .beautifulplots import plot_defaults 
+from .beautifulplots import plot_defaults
 from .beautifulplots import set_axisparams
 from .beautifulplots import get_kwargs
+from .beautifulplots import set_yaxis_format
 from .barplot import barplot
 from .lineplot import lineplot
